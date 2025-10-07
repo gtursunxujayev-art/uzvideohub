5948416388ea4455d192e1bdeafc2f21b07b5d73
gtursunxujayev-art <gtursunxujayev@gmail.com>
2025-10-08 00:43:44 +0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
+}
+
+export default nextConfig
