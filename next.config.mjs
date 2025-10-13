d6f7f40f48a07b90d64718838111829902d07a17
gtursunxujayev-art <gtursunxujayev@gmail.com>
2025-10-14 00:05:17 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // Turn OFF typedRoutes so <Link href={string}> is allowed everywhere.
-  // This fixes: "Type 'string' is not assignable to type 'UrlObject | RouteImpl<string>'"
   experimental: {
-    typedRoutes: false
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
+}
 
-  env: {
-    NEXT_PUBLIC_APP_NAME: "Sotuv bo'limi",
-    NEXT_PUBLIC_APP_URL: "https://sotuv-bolimi-kappa.vercel.app"
-  },
-  poweredByHeader: false
-};
-
-export default nextConfig;
+export default nextConfig
